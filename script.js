330d06c8f667a77556386ab9c06a6b5ac175dd2d
Bhindnaresh <tomarankitsingh2000@gmail.com>
2024-05-03 11:19:26 +0530

    added contact form

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbzJEpaC83emJ98Wfsf3imlULXu8bzk8WkJiWNZBBylOuJmnAQ8ZdWxQv2b1iK8XYYgv/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+          msg.innerHTML=""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
