7870a866ff2d1387673407292e10e32e6797c04e
Bhindnaresh <tomarankitsingh2000@gmail.com>
2024-05-05 14:44:27 +0530

    making website responsive

--- script.js
+++ script.js
@@ -5,19 +5,20 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
- const scriptURL = 'https://script.google.com/macros/s/AKfycbzJEpaC83emJ98Wfsf3imlULXu8bzk8WkJiWNZBBylOuJmnAQ8ZdWxQv2b1iK8XYYgv/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById("msg")
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzJEpaC83emJ98Wfsf3imlULXu8bzk8WkJiWNZBBylOuJmnAQ8ZdWxQv2b1iK8XYYgv/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML = "Message sent successfully"
-        setTimeout(function(){
-          msg.innerHTML=""
-        }, 5000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
